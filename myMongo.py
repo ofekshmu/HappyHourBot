2cad2270a961b5c5c5d604ef5ff77395409bcc1f
ofekshmu <ofek.shmuel1@gmail.com>
2022-08-30 21:01:46 +0300

    implemented mongo class

--- myMongo.py
+++ myMongo.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from xml.dom import NotFoundErr
 import certifi
 from pymongo import MongoClient
 import json
@@ -12,11 +14,9 @@ class Mongo:
         password = dict["DBpassword"]
         user_name = dict["user_name"]
         ca = certifi.where()
-        #cluster = MongoClient(f"mongodb://{user_name}:{password}@127.0.0.1:27017/dbname?keepAlive=true&poolSize=30&autoReconnect=true&socketTimeoutMS=360000&connectTimeoutMS=360000")
         cluster = MongoClient(f"mongodb+srv://{user_name}:{password}@cluster0.liy1wfj.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
         self.db = cluster["happy_hour"]
-        self.section = self.db["section"]
-        self.rounds = self.db["rounds"]
+
 
     def load_initial_db(self):
         dict = json.load(open('config.json'))
@@ -36,16 +36,31 @@ class Mongo:
             the user's mail
         """
         # check if id is unique
-        if self.section.count_documents({"_id": id}, limit=1):
+        if self.db['section'].count_documents({"_id": id}, limit=1):
             return False
-        self.section.insert_one({"_id": id, "name": name, "mail": mail})
+        self.db['section'].insert_one({"_id": id, "name": name, "mail": mail})
         return True
 
-    def delete_user(self, name):
-        pass
-
-    def update_user(self, name, mail = None, phone = None):
-        pass
+    def get_users(self, type):
+        """Returns entire user data according to type parameter
+        for 'all' -> returns 2 dictionaries; keva and sadir
+        for 'sadir'/'keva' -> returns a dictionay 
+         Parameters
+        ----------
+        type : str
+            indicate what type of users to return - 'sadir' or 'keva'
+            values allowed are 'sadir'/'keva'/'all'
+        """
+        match type:
+            case 'all':
+                return self.db['section']
+            case 'sadir':
+                return self.db['section']['Sadir']
+            case 'keva':
+                return self.db['section']['Keva']
+            case other:
+                raise ValueError(f"O-Error: Wrong input argument ({type}) in get_users\n\
+                                  Should be 'sadir'/'keva'/'all'")
 
     def insert_round(self, name_lst):
         """Insert a new, random, name list to the database
@@ -55,14 +70,28 @@ class Mongo:
         name_lst : List
             A randomly sorted list of names.
         """
-        self.rounds({"date": datetime.now(), "queue": name_lst })
+        self.db['rounds'].insert_one({"_id": datetime.now(), "queue": name_lst })
 
     def get_round(self, date):
-        pass
-        #self.rounds.find
+        """ get a specific round upon a given date
+        
+        Parameters
+        ----------
+        date : datetime
+            a date of format ??????
+            the date should fit the start date of the relevant round
+        """
+        
+        if self.db['rounds'].count_documents(date, limit=1):
+            return self.db['rounds'].find_one(date)
+        else:
+            raise KeyError(f"O-Error: the date {date} is not found in data 'section'.")
 
     def get_recent_round(self):
-        pass
+        if self.db['rounds'].count_documents(limit=1):
+            return self.db['rounds'].find().sort({'_id':-1}).limit(1)
+        else:
+            raise NotFoundErr(f"O-Error: No rounds in data base.")
 
 mymongo = Mongo()
 mymongo.load_initial_db()
